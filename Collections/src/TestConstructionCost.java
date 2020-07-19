a043312e97fc9152964859d4775a1fe997680658
Admin <Admin@192.168.0.6>
2020-07-19 18:34:30 +0530

    collections

---
+++ Collections/src/TestConstructionCost.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestConstructionCost {
+
+	public void testCost() {
+		CalculateCost cc = new CalculateCost();
+		assertEquals(cc,cc.getCost("standard",6,false));
+		assertEquals(cc,cc.getCost("highStandard",3,true));
+		assertEquals(cc,cc.getCost("aboveStandard",2,true));
+		assertEquals(cc,cc.getCost("highStandard",5,false));
+	}
+
+}
+
---
+++ Collections/src/calculate.java
@@ -0,0 +1,15 @@
+
+public class calculate {
+	public double getCost(String materialStandard,double area,boolean automated) {
+		double cost = 0.0;
+		if(materialStandard.equals("standard"))
+			cost= 1200*area;
+		else if(materialStandard.equals("aboveStandard"))
+			cost = 1500*area;
+		else if(materialStandard.equals("highStandard") && automated == true)
+			cost = 1800*area;
+		else if(materialStandard.equals("highStandard"))
+			cost = 1800*area;
+		return cost;
+	}
+}
